c23f112d4d9e1a12cd1c2450102b04723d14e589
Khangte <115693185+Khangte@users.noreply.github.com>
2024-05-23 23:31:45 +0900

    Create BombGenerator.cs

---
+++ BombGenerator.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombGenerator : MonoBehaviour
+{
+    public GameObject bombPrefab; // 생성할 폭탄 프리팹
+    float span = 5.3f;            // 폭탄 생성 주기 (5.3초마다 생성)
+    float delta = 0;              // 경과 시간을 측정하기 위한 변수
+
+    void Update()
+    {
+        // 경과 시간을 누적
+        this.delta += Time.deltaTime;
+        
+        // 주기마다 폭탄 생성
+        if (this.delta > this.span)
+        {
+            // 경과 시간 초기화
+            this.delta = 0;
+
+            // 폭탄 프리팹을 복제하여 씬에 생성
+            GameObject go = Instantiate(bombPrefab) as GameObject;
+
+            // 폭탄을 불규칙하게 생성하기 위해 x좌표를 무작위로 설정
+            int px = Random.Range(-6, 7);   // x좌표 -6, 6 사이에 불규칙하게 위치
+            go.transform.position = new Vector3(px, 7, 0); // 위에서 아래로 폭탄이 낙하
+        }
+    }
+}
